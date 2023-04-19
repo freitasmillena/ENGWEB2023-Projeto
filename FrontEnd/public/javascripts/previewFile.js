b7a34864cf4a5f1bfe85ea6ec6111f8a61c9820c
Millena Freitas <59456180+freitasmillena@users.noreply.github.com>
2023-04-19 15:18:47 +0100

    password validation frontend

--- FrontEnd/public/javascripts/previewFile.js
+++ FrontEnd/public/javascripts/previewFile.js
@@ -1,3 +1,132 @@
+function checkPasswordStrength(password) {
+    let strength = 0;
+
+    //At least 8 chars
+    if(password.length >= 8) strength ++;
+
+    //At least one uppercase letter
+    if(/[A-Z]/.test(password)) strength ++;
+
+    //At least one lowercase letter
+    if(/[a-z]/.test(password)) strength ++;
+
+    //At least one number
+    if(/\d/.test(password)) strength ++;
+
+    //At least one special character
+    if(/[^A-Za-z0-9]/.test(password)) strength ++;
+
+    return strength;
+
+}
+
+function updateStrengthMeter(password){
+    const strengthMeter = document.getElementById('strengthMeter');
+    const strengthLevel = document.getElementById('strengthLevel');
+    const strength = checkPasswordStrength(password);
+
+    strengthMeter.style.width = `${(strength / 10) * 100}%`
+
+    let strengthText = '';
+
+    if(strength == 0){
+    
+    }
+    
+    else if(strength < 3) {
+        strengthMeter.style.backgroundColor = '#FF4D4D';
+        strengthText = 'Weak';
+    
+    }
+    
+    else if(strength < 4) {
+        strengthMeter.style.backgroundColor = '#FFC300';
+        strengthText = 'Medium';
+    }
+
+    else if(strength < 5) {
+        strengthMeter.style.backgroundColor = '#87C55F';
+        strengthText = 'Strong';
+    }
+
+    else {
+        strengthMeter.style.backgroundColor = '#4CAF50';
+        strengthText = 'Very Strong';
+    }   
+
+    strengthLevel.textContent = strengthText;
+
+
+}
+
+function updatePasswordRequirements(password){
+    const lengthReq = document.getElementById('lengthReq');
+    const uppercaseReq = document.getElementById('uppercaseReq');
+    const lowercaseReq = document.getElementById('lowercaseReq');
+    const numberReq = document.getElementById('numberReq');
+    const specialCharReq = document.getElementById('specialCharReq');
+    const passwordMatchReq = document.getElementById('passwordMatchReq');
+    const passwordInput = document.getElementById('passwordInput');
+    const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+
+    lengthReq.style.color = password.length >= 8 ? '#4CAF50' : '#FF4D4D';
+    uppercaseReq.style.color = /[A-Z]/.test(password) ? '#4CAF50' : '#FF4D4D';
+    lowercaseReq.style.color = /[a-z]/.test(password) ? '#4CAF50' : '#FF4D4D';
+    numberReq.style.color = /\d/.test(password) ? '#4CAF50' : '#FF4D4D';
+    specialCharReq.style.color = /[^A-Za-z0-9]/.test(password) ? '#4CAF50' : '#FF4D4D';
+
+    console.log(confirmPasswordInput.value)
+
+    if(confirmPasswordInput.value === password && password != '') {
+        passwordMatchReq.style.color = '#4CAF50';
+    }
+    else {
+        passwordMatchReq.style.color = '#FF4D4D';
+    }
+
+    if(checkPasswordStrength(password) == 5) passwordInput.setCustomValidity('');
+    else passwordInput.setCustomValidity('Password does not meet the requirements.');
+
+    
+}
+
+function confirmPasswordMatch() {
+    const passwordInput = document.getElementById('passwordInput');
+    const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+
+    if(passwordInput.value === confirmPasswordInput.value) {
+        confirmPasswordInput.setCustomValidity('');
+    }
+    else {
+        confirmPasswordInput.setCustomValidity('Passwords do not match.');
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded',() => {
+    const passwordInput = document.getElementById('passwordInput');
+    const confirmPasswordInput = document.getElementById('confirmPasswordInput');
+    let password = '';
+    passwordInput.addEventListener('input', (event) => {
+        password = event.target.value;
+        updateStrengthMeter(password);
+        updatePasswordRequirements(password);
+        confirmPasswordMatch();
+    });
+
+    confirmPasswordInput.addEventListener('input', () => {
+        confirmPasswordMatch();
+        updatePasswordRequirements(password);
+    });
+})
+
+
+
+
+
+
+
 function formatFileSize(size){
     const units = ['B', 'KB', 'MB', 'GB', 'TB'];
     let unitIndex = 0;
